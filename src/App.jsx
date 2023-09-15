ada341dbaeaef568902b6e27073e03a4f3422062
Waleed_Ahmed <0100waleedahmed0100@gmail.com>
2023-09-15 18:40:31 +0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+    <div className="min-h-screen bg-slate-800 flex justify-center items-center">
+      <h1 className="text-4xl text-teal-300">WDWaleed</h1>
+      </div>      
+    </>
+  )
+}
+
+export default App
