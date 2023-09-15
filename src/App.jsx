135a36173a8cc198a3ab9f477e170014c4656285
Waleed_Ahmed <0100waleedahmed0100@gmail.com>
2023-09-15 18:52:32 +0500

    Status Update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <>
     <div className="min-h-screen bg-slate-800 flex justify-center items-center">
-      <h1 className="text-4xl text-teal-300">WDWaleed</h1>
+      <h1 className="text-3xl text-teal-300 w-2/3 text-center leading-[1.25] font-normal ">
+      A <span className="font-medium">Front End Web Developer</span>, utilizing the powers of HTML, CSS, JavaScript, <strong>React</strong>, and <strong>Tailwind CSS</strong>, to create visually appealing and highly interactive user interfaces. Currently, I am in the process of creating wonderful projects to showcase on this portfolio website of mine.
+      </h1>
       </div>      
     </>
   )
